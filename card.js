48f7415fb4ce0cede2f95a2e6f9614d5a8e0cac4
miqo221 <miqonazarian@gmail.com>
2022-01-28 12:25:40 +0400

    changes by comments

--- card.js
+++ card.js
@@ -1,22 +1,23 @@
 
 let array = [];
+let number = 0;
+let id = "";
 
 function randomNumberGenerator(min = 1, max = 1000) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 function randomIdGenerator() {
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
 }
 
 function sortArray() {
-    array = array.sort(function (a, b) { return a.value - b.value });
+    array = array.sort((a, b) => { return a.value - b.value });
 }
 
-const newArray = [];
-
 function createDiv() {
-    const number = randomNumberGenerator(1, 1000);
-    const id = randomIdGenerator();
+    number = randomNumberGenerator(1, 1000);
+    id = randomIdGenerator();
     const newCard = document.createElement('div');
     newCard.classList.add("card");
     newCard.innerHTML = number;
@@ -27,15 +28,15 @@ function createDiv() {
     closeButton.innerHTML = '&times';
     newCard.appendChild(closeButton);
 
-    closeButton.addEventListener("click", myFunction);
-    function myFunction() {
+    closeButton.addEventListener("click", clickHandler);
+    function clickHandler() {
         newCard.parentElement.removeChild(newCard);
         const divID = this.parentNode.id;
-         for(let j = 0; j < array.length;j++) {
-             if(array[j].id === divID) {
-                 array.splice(array[j],1);
-             }
-         }
+        for (let j = 0; j < array.length; j++) {
+            if (array[j].id === divID) {
+                array.splice(array[j], 1);
+            }
+        }
     };
 
     const cardObject = {
@@ -50,21 +51,19 @@ function addCard() {
     createDiv()
 }
 
-function removeAll() {
+
+function removeDom() {
     const items = document.getElementsByClassName('card');
     for (let i = 0; i < array.length; i++) {
         while (items.length > 0) {
             items[i].parentNode.removeChild(items[i])
         }
     }
-    array.length = 0;
 }
 
-function removeDom() {
-    const elements = document.querySelectorAll(".card");
-    for (let i = 0; i < array.length; i++) {
-        elements[i].parentNode.removeChild(elements[i]);
-    }
+function removeAll() {
+    removeDom();
+    array.length = 0;
 }
 
 function sortDivs() {
@@ -79,9 +78,15 @@ function sortDivs() {
         closeButton.classList.add('closebtn');
         closeButton.innerHTML = '&times';
         divs.appendChild(closeButton);
-        closeButton.addEventListener("click", myFunction);
-        function myFunction() {
+        closeButton.addEventListener("click", clickHandler);
+        function clickHandler() {
             divs.parentElement.removeChild(divs);
+            const divID = this.parentNode.id;
+            for (let j = 0; j < array.length; j++) {
+                if (array[j].id === divID) {
+                    array.splice(array[j], 1);
+                }
+            }
         };
     }
 }
